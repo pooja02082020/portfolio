84cd99b3332e34dc3681bf982de2b14071ba0dd1
pooja02082020 <lohanipooja77@gmail.com>
2025-12-02 12:33:59 -0500

    added new resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,7 @@ const About = () => (
   <section id="about" className="section">
     <h2>About Me</h2>
     <p>
-      I’m a Software Developer and QA Engineer with over 3 years of hands-on
-      experience in automation and full-stack development. I’m passionate about
-      creating efficient, user-friendly web solutions and constantly learning new technologies.
+      Results-driven Software Developer & SDET with 6+ years of strong proficiency in Java, React, Automation Testing, and Cloud technologies. Experienced in building scalable full-stack applications, designing automation frameworks, and improving software quality through rigorous testing and CI/CD practices. Adept at solving complex problems, optimizing performance, and collaborating within Agile teams. Seeking a role where I can contribute to high-impact products by applying my expertise in full-stack development, test automation, and cloud-based systems.
     </p>
   </section>
 );
