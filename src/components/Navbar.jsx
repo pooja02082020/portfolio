dbeb9643814a11c127a3c0a66daa1daf59c3004f
pooja02082020 <lohanipooja77@gmail.com>
2026-01-24 06:07:27 +0545

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
           <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#skills">Skills</a></li>
-          <li><a href="#experience">Experience</a></li>
+          <li><a href="#experience">Experiences</a></li>
           <li><a href="#contact" className="contact-btn">Contact</a></li>
         </ul>
       </div>
